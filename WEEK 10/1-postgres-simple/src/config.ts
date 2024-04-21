bd7fcb2360c1e1ff1db7706096999d6bec3d3ebe
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-04-21 12:41:30 +0530

    fix: Change config

--- WEEK 10/1-postgres-simple/src/config.ts
+++ WEEK 10/1-postgres-simple/src/config.ts
@@ -1,3 +1,2 @@
 
-export const DB_URL =
-	'postgresql://Whales_owner:go9OqjfDU5Fi@ep-dawn-breeze-a52exogq.us-east-2.aws.neon.tech/100x?sslmode=require';
+export const DB_URL = "postgres://postgres:mysecretpassword@localhost/postgres";
